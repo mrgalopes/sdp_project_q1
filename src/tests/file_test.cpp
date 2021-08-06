f837b4dab5ade74bd301d1f2f120ae4eae322628
Vitor Kiguchi <vitor-kiguchi@hotmail.com>
2021-08-06 15:21:32 -0300

    Add edge count test

--- src/tests/file_test.cpp
+++ src/tests/file_test.cpp
@@ -12,5 +12,13 @@ TEST_CASE("The resulting should have the correct number of verticies", "[file_io
     graphFile.close();
 
     REQUIRE(graph.vertices.size() == graph.numVertices);
+
+    unsigned int sumEdges = 0;
+
+    for (auto &t: graph.vertices) {
+        sumEdges += t.getEdgeList().size();
+    }
+
+    REQUIRE(graph.numEdges == sumEdges/2);
 }
 
