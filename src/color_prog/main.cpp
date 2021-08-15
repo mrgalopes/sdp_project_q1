b16f1e18758765fda3323fd579b1ba4cade59b15
VBenevides <massaki1999@gmail.com>
2021-08-15 16:25:54 -0300

    fixed main

--- src/color_prog/main.cpp
+++ src/color_prog/main.cpp
@@ -20,7 +20,7 @@ int main() {
     Graph graph;
     // READING FILE AND CONSTRUCTING GRAPH
     std::ifstream graphFile("../../dist/graphs/rgg_n_2_15_s0.graph");
-    IOM::loadGraph(graph, graphFile);
+    IOM::loadGraphThreaded(graph, graphFile);
     graphFile.close();
 
     // TESTING COLORING ALGORITHM
@@ -30,6 +30,6 @@ int main() {
     std::cout << "Number of vertices in file: " << graph.numVertices << std::endl;
     std::cout << "Number of vertices created: " << graph.vertices.size() << std::endl;
     std::cout << "\n -- END OF TEST --" << std::endl;
-    delete coloringAlgorithm;
+    //delete coloringAlgorithm;
     return 0;
 }
