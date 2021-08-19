11c76caa3877d90fb2132cc1f97d167d16621aab
Vitor Kiguchi <vitor-kiguchi@hotmail.com>
2021-08-19 17:49:13 -0300

    Luby: use a bitset and a vector instead of a set

--- src/core/LubyColoringAlgorithm.cpp
+++ src/core/LubyColoringAlgorithm.cpp
@@ -13,25 +13,26 @@
 
 namespace {
 std::mutex mtx;
-void independentSetWorker(const std::vector<Vertex>& vertices, const std::unordered_set<int>& A,
-                          std::shared_ptr<std::vector<int>> r_p,
-                          const std::unordered_set<int>::const_iterator A_begin,
-                          const std::unordered_set<int>::const_iterator A_end,
-                          std::unordered_set<int>& i_set, std::unordered_set<int>& x_set) {
-    std::unordered_set<int> i_set_prime;
-    std::unordered_set<int> x_set_prime;
+void independentSetWorker(const std::vector<Vertex>& vertices, const std::vector<bool>& bitset_A, const std::vector<unsigned int>& A,
+                          std::shared_ptr<std::vector<unsigned int>> r_p,
+                          const std::size_t A_begin,
+                          const std::size_t A_end,
+                          std::unordered_set<unsigned int>& i_set, std::unordered_set<unsigned int>& x_set) {
+    std::unordered_set<unsigned int> i_set_prime;
+    std::unordered_set<unsigned int> x_set_prime;
 
-    for (auto v = A_begin; v != A_end; v++) {
+    for (auto j = A_begin; j != A_end; j++) {
+        const auto v = A[j];
         bool peak = true;
-        const auto& edge_list = vertices.at(*v - 1).getEdgeList();
+        const auto& edge_list = vertices.at(v - 1).getEdgeList();
         for (auto vx : edge_list) {
-            if (A.contains(vx) && r_p->at(vx - 1) > r_p->at(*v - 1)) {
+            if (bitset_A[v-1] && r_p->at(vx - 1) > r_p->at(v - 1)) {
                 peak = false;
                 break;
             }
         }
         if (peak) {
-            i_set_prime.insert(*v);
+            i_set_prime.insert(v);
             x_set_prime.insert(edge_list.cbegin(), edge_list.cend());
         }
     }
@@ -44,20 +45,24 @@ void independentSetWorker(const std::vector<Vertex>& vertices, const std::unorde
 } // Anonymous namespace
 
 void LubyColoringAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
-    std::size_t size_u = vertices.size();
-    int lowest_available_color = 1;
+    const std::size_t size_u = vertices.size();
+    unsigned int lowest_available_color = 1;
 
-    auto r_p = std::make_shared<std::vector<int>>(size_u); // random permutation
+    auto r_p = std::make_shared<std::vector<unsigned int>>(size_u); // random permutation
     std::iota(r_p->begin(), r_p->end(), 1);
 
-    std::unordered_set<int> U{r_p->begin(), r_p->end()}; // U contains the IDs of uncolored vertices
-    std::unordered_set<int> A{r_p->begin(), r_p->end()}; // A contains the IDs of undecided vertices
+    std::vector<unsigned int> U{r_p->begin(), r_p->end()}; // U contains the IDs of uncolored vertices
+    std::vector<unsigned int> A; // A contains the IDs of undecided vertices
 
-    std::unordered_set<int> i_set{};
-    std::unordered_set<int> x_set{};
+    std::vector<bool> bitset_U(size_u, true);
+    std::vector<bool> bitset_A(size_u, true);
+
+    std::unordered_set<unsigned int> i_set{};
+    std::unordered_set<unsigned int> x_set{};
 
     while (!U.empty()) {
         A = U;
+        bitset_A = bitset_U;
         i_set.clear();
         while (!A.empty()) {
             std::shuffle(r_p->begin(), r_p->end(), std::default_random_engine(_seed));
@@ -66,12 +71,10 @@ void LubyColoringAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
             // choose independent set
             std::vector<std::thread> workers;
             for (int i = 0; i < this->_numWorkers; i++) {
-                auto A_begin = A.cbegin();
-                std::advance(A_begin, (i * A.size() / this->_numWorkers));
-                auto A_end = A.cbegin();
-                std::advance(A_end, ((i + 1) * A.size() / this->_numWorkers));
+                auto A_begin = (i * A.size()) / this->_numWorkers;
+                auto A_end = ((i + 1) * A.size()) / this->_numWorkers;
 
-                workers.emplace_back(independentSetWorker, std::ref(vertices), std::ref(A), r_p,
+                workers.emplace_back(independentSetWorker, std::ref(vertices), std::ref(bitset_A), std::ref(A), r_p,
                                      A_begin, A_end, std::ref(i_set), std::ref(x_set));
             }
             for (auto& t : workers) {
@@ -79,20 +82,20 @@ void LubyColoringAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
             }
 
             for (auto& i : i_set) {
-                A.erase(i);
+                bitset_A[i-1] = false;
             }
             for (auto& x : x_set) {
-                A.erase(x);
+                bitset_A[x-1] = false;
             }
+            std::erase_if(A, [=](int v){return bitset_A[v-1]!=true;});
         }
 
         for (auto v : i_set) {
             vertices.at(v - 1).setColor(lowest_available_color);
+            bitset_U[v-1] = false;
         }
 
         lowest_available_color += 1;
-        for (auto& i : i_set) {
-            U.erase(i);
-        }
+        std::erase_if(U, [=](int v){return bitset_U[v-1]!=true;});
     }
 }
