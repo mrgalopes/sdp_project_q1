093f36200e84e10157bbf3a7e02e6330d171abcf
Vitor Kiguchi <vitor-kiguchi@hotmail.com>
2021-08-19 18:14:18 -0300

    Jones: clang format, const qualify and small improvements

--- src/core/JonesPlassmannAlgorithm.cpp
+++ src/core/JonesPlassmannAlgorithm.cpp
@@ -11,19 +11,20 @@
 
 namespace {
 std::mutex mtx;
-void JPWorker(std::vector<Vertex>& vertices, std::vector<bool>& bitset_U, std::vector<unsigned int>& U,
-              std::shared_ptr<std::vector<unsigned int>> r_p,
-              std::size_t U_begin,
-              std::size_t U_end, std::unordered_set<unsigned int>& i_set) {
+void JPWorker(std::vector<Vertex>& vertices, const std::vector<bool>& bitset_U,
+              const std::vector<unsigned int>& U, std::shared_ptr<std::vector<unsigned int>> r_p,
+              const std::size_t U_begin, const std::size_t U_end,
+              std::unordered_set<unsigned int>& i_set) {
 
     unsigned int i = 0;
-    std::unordered_set<unsigned int> i_set_prime; // independent set for the subgraph received by the worker
+    std::unordered_set<unsigned int>
+        i_set_prime; // independent set for the subgraph received by the worker
     for (auto j = U_begin; j != U_end; j++) {
         const auto v = U[j];
         bool peak = true;
         const auto& edge_list = vertices.at(v - 1).getEdgeList();
         for (auto vx : edge_list) {
-            if (bitset_U[vx-1] && r_p->at(vx - 1) > r_p->at(v - 1)) {
+            if (bitset_U[vx - 1] && r_p->at(vx - 1) > r_p->at(v - 1)) {
                 peak = false;
                 break;
             }
@@ -53,19 +54,19 @@ void JPWorker(std::vector<Vertex>& vertices, std::vector<bool>& bitset_U, std::v
 
 void JonesPlassmannAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
     const std::size_t size_u = vertices.size();
-    std::size_t count_u = size_u;
 
     auto r_p = std::make_shared<std::vector<unsigned int>>(size_u); // random permutation
-    std::iota(r_p->begin(), r_p->end(), 1);                // r_p has all the ids
+    std::iota(r_p->begin(), r_p->end(), 1);                         // r_p has all the ids
 
-    std::vector<unsigned int> U{r_p->begin(), r_p->end()}; // U contains the IDs of uncolored vertices
+    std::vector<unsigned int> U{r_p->begin(),
+                                r_p->end()};  // U contains the IDs of uncolored vertices
     std::vector<bool> bitset_U(size_u, true); // bitset
 
     // randomly shuffle the values in r_p to use as weights
     std::shuffle(r_p->begin(), r_p->end(), std::default_random_engine(_seed));
 
     std::unordered_set<unsigned int> i_set{};
-    while (count_u > 0) {
+    while (!U.empty()) {
         i_set.clear();
 
         // choose independent set and color it
@@ -74,17 +75,16 @@ void JonesPlassmannAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
             auto U_begin = (i * U.size()) / this->_numWorkers;
             auto U_end = ((i + 1) * U.size()) / this->_numWorkers;
 
-            workers.emplace_back(JPWorker, std::ref(vertices), std::ref(bitset_U), std::ref(U), r_p, U_begin, U_end,
-                                 std::ref(i_set));
+            workers.emplace_back(JPWorker, std::ref(vertices), std::cref(bitset_U), std::cref(U),
+                                 r_p, U_begin, U_end, std::ref(i_set));
         }
         for (auto& t : workers) {
             t.join();
         }
 
         for (auto& v : i_set) {
-            bitset_U[v-1] = false;
-            count_u--;
+            bitset_U[v - 1] = false;
         }
-        std::erase_if(U, [=](int v){return bitset_U[v-1]!=true;});
+        std::erase_if(U, [bitset_U](int v) { return bitset_U[v - 1] != true; });
     }
 }
