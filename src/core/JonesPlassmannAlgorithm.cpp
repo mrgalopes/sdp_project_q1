151939bad699c9a3824d02014a8688ff36151a61
Gabriel Lopes Rodrigues <gabriellopes@usp.br>
2021-08-28 16:07:40 -0300

    Improves Largest Degree First and Jones Plassman Algorithms

--- src/core/JonesPlassmannAlgorithm.cpp
+++ src/core/JonesPlassmannAlgorithm.cpp
@@ -11,13 +11,13 @@
 namespace {
 std::mutex mtx;
 void JPWorker(std::vector<Vertex>& vertices, const std::vector<bool>& bitset_U,
-              const std::vector<unsigned int>& U, std::shared_ptr<std::vector<unsigned int>> r_p,
-              const std::size_t U_begin, const std::size_t U_end,
-              std::unordered_set<unsigned int>& i_set) {
+              const std::vector<unsigned int>& U,
+              const std::shared_ptr<std::vector<unsigned int>>& r_p, const std::size_t U_begin,
+              const std::size_t U_end, std::unordered_set<unsigned int>& i_set) {
 
     unsigned int i = 0;
     std::unordered_set<unsigned int>
-        i_set_prime; // independent set for the subgraph received by the worker
+        i_set_prime{}; // independent set for the subgraph received by the worker
     for (auto j = U_begin; j != U_end; j++) {
         const auto v = U[j];
         bool peak = true;
@@ -79,7 +79,7 @@ void JonesPlassmannAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
             auto U_end = ((i + 1) * U.size()) / this->_numWorkers;
 
             workers.emplace_back(JPWorker, std::ref(vertices), std::cref(bitset_U), std::cref(U),
-                                 r_p, U_begin, U_end, std::ref(i_set));
+                                 std::cref(r_p), U_begin, U_end, std::ref(i_set));
         }
         for (auto& t : workers) {
             t.join();
@@ -88,6 +88,6 @@ void JonesPlassmannAlgorithm::colorGraph(std::vector<Vertex>& vertices) {
         for (auto& v : i_set) {
             bitset_U[v - 1] = false;
         }
-        std::erase_if(U, [bitset_U](int v) { return bitset_U[v - 1] != true; });
+        std::erase_if(U, [bitset_U](int v) { return !bitset_U[v - 1]; });
     }
 }
